6a9c568f19b7724ba41f421c1f11dc12470ce7e6
= <=>
2016-04-27 21:35:49 -0300

    exercicio 5

---
+++ questao_05.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+
+	float volume = 0.0f;
+	float raio = 0.0f
+
+
+
+	
+	cout << "\t Digite o valor do raio: " << std::endl << "\t";
+	cin >> raio;
+
+	cout << "--------------------------------------" << std::endl;
+
+	v=(4*3.14*r*r*r)/3;
+
+	cout << std::endl;
+	cout << "---------------------------" << std::endl;
+	cout << " o volume e:\t" << volume << std::endl;
+	cout << "---------------------------" << std::endl;
+
+} 
